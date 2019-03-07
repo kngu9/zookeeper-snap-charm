9df26a35830d0149e9dce64b668de9d5d20de349
kngu9 <me@khanh.pw>
2019-03-07 16:22:43 -0600

    Fixed file rerouting

--- snap/local/zkCli-wrapper.sh
+++ snap/local/zkCli-wrapper.sh
@@ -3,8 +3,8 @@
 set -eu
 
 export ZOO_LOG_DIR=$SNAP_USER_DATA/var/log/zookeeper
-export ZOO_CFG_DIR=$SNAP_USER_DATA/etc/zookeeper
+export ZOOCFGDIR=$SNAP_USER_DATA/etc/zookeeper
 export PATH=$SNAP/usr/lib/jvm/default-java/bin:$PATH
 unset JAVA_HOME
 
-$SNAP/opt/zookeeper/bin/zkCli.sh $@
+$SNAP/opt/zookeeper/bin/zkCli.sh "$@"
--- snap/local/zookeeper-wrapper.sh
+++ snap/local/zookeeper-wrapper.sh
@@ -6,16 +6,15 @@ export PATH=$SNAP/usr/lib/jvm/default-java/bin:$PATH
 unset JAVA_HOME
 
 export ZOO_LOG_DIR=$SNAP_USER_DATA/var/log/zookeeper
-export ZOO_CFG_DIR=$SNAP_USER_DATA/etc/zookeeper
+export ZOOCFGDIR=$SNAP_USER_DATA/etc/zookeeper
 
 mkdir -p $ZOO_LOG_DIR
-mkdir -p $ZOO_CFG_DIR
+mkdir -p $ZOOCFGDIR
 
 touch $ZOO_LOG_DIR/zookeeper.out
 
 for file in $SNAP/opt/zookeeper/conf/*; do
-    [ ! -f $ZOO_CFG_DIR/$(basename $file) ] && cp $file $ZOO_CFG_DIR/$(basename $file)
+    [ ! -f $ZOOCFGDIR/$(basename $file) ] && cp $file $ZOOCFGDIR/$(basename $file)
 done
 
-
-$SNAP/opt/zookeeper/bin/zkServer.sh $@
+$SNAP/opt/zookeeper/bin/zkServer.sh "$@"
