4ad6e4911964d98ab9ac4904821e0136b44d0791
kngu9 <me@khanh.pw>
2019-03-08 21:39:10 -0600

    ZK now uses SASL

--- snap/local/zookeeper-wrapper.sh
+++ snap/local/zookeeper-wrapper.sh
@@ -5,16 +5,21 @@ set -eu
 export PATH=$SNAP/usr/lib/jvm/default-java/bin:$PATH
 unset JAVA_HOME
 
-export ZOO_LOG_DIR=$SNAP_USER_DATA/var/log/zookeeper
-export ZOOCFGDIR=$SNAP_USER_DATA/etc/zookeeper
+export ZOO_LOG_DIR=$SNAP_COMMON/var/log/zookeeper
+export ZOOCFGDIR=$SNAP_COMMON/etc/zookeeper
 
 mkdir -p $ZOO_LOG_DIR
 mkdir -p $ZOOCFGDIR
 
 touch $ZOO_LOG_DIR/zookeeper.out
 
-for file in $SNAP/opt/zookeeper/conf/*; do
+mkdir -p $SNAP_COMMON/data/zookeeper
+
+for file in $SNAP/etc/zookeeper/*; do
     [ ! -f $ZOOCFGDIR/$(basename $file) ] && cp $file $ZOOCFGDIR/$(basename $file)
 done
+
+export ZOO_LOG4J_PROP="TRACE, stdout, stderr"
+export JVMFLAGS="-Djava.security.auth.login.config=$ZOOCFGDIR/zookeeper-jaas.conf"
 
 $SNAP/opt/zookeeper/bin/zkServer.sh "$@"
